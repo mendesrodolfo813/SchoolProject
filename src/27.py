2ba028d3ba1a9c59e7ba0af9752e68f8d25eb3d1
mendesrodolfo813 <mendesrodolfo813@gmail.com>
2025-04-08 22:13:49 +0000

    Add 27.py

---
+++ src/27.py
@@ -0,0 +1,9 @@
+class MyClass:
+    def __init__(self):
+        pass
+    
+    def do_something(self):
+        print("This is an example method.")
+
+my_instance = MyClass()
+my_instance.do_something()
